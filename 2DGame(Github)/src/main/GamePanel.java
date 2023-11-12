a793c682170d2f5f9efecb3e47677d6930e6146d
Furqan <furqanazmat@gmail.com>
2023-11-11 20:33:33 -0500

    Added buttons to draw/erase game

--- 2DGame(Github)/src/main/GamePanel.java
+++ 2DGame(Github)/src/main/GamePanel.java
@@ -2,7 +2,11 @@ package main;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import inputs.KeyboardInputs;
@@ -20,13 +24,47 @@ public class GamePanel extends JPanel {
 	//global vars 
 	private MouseInputs mouseInputs;
 	private Game game;
-	
+	boolean isDraw = false;
 	
 	public GamePanel(Game game) { // game panel constructor
 		
+		
 		mouseInputs = new MouseInputs(this);
 		this.game = game;
-	
+		
+		JFrame frm = new JFrame("Main Menu");
+		JPanel content = new JPanel();
+		frm.add(content);
+		
+		JButton draw = new JButton("Draw");
+		JButton stop = new JButton("Stop");
+		content.add(draw);
+		content.add(stop);
+		frm.setSize(300, 100);
+		frm.setLocationRelativeTo(null) ;
+		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frm.setVisible(true) ;
+		
+		draw.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                isDraw = true;
+            }
+        });
+		
+		stop.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	 isDraw = false;
+                 repaint();
+
+                try {
+                    Thread.sleep(100); // Add a short delay to allow Swing to update the UI
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+		
+		
 		setPaneSize(); //calls the method to set window size
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs); // press, release, click, enter, and exit
@@ -46,9 +84,15 @@ public class GamePanel extends JPanel {
 	}
 	
 	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		game.render(g);
+		super.paintComponent(g); 
+
+	    if (isDraw) {
+	        game.render(g);
+	    }
+	
 	}
+	
+	
 	
 	public Game getGame() {
 		return game;
