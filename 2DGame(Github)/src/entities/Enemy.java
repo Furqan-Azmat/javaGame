c36528e8678fdd3042f8dbb08ec0a02d1ece4058
furqa <furqanazmat@gmail.com>
2023-11-17 16:22:46 -0500

    fixed small error

--- 2DGame(Github)/src/entities/Enemy.java
+++ 2DGame(Github)/src/entities/Enemy.java
@@ -3,16 +3,36 @@ package entities;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
+import javax.swing.JPanel;
+
 import utils.LoadSave;
 
 public abstract class Enemy extends Entity{
-	BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.ENEMYATLAS);
-	//private BufferedImage enemy1, enemy2, enemy3;
-	public Enemy(float x, float y, int width, int height) {
-		super(x, y, width, height);
-		initializeHitbox(x, y, width, height);	
-	}
-	
-	public abstract void drawCharacter(Graphics g); // abstract method forces implementation of enemy appearance for each enemy type
-	
+    BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.ENEMYATLAS);
+    private float locationA;
+    private float locationB;
+    private boolean forward = true;
+
+    public Enemy(float x, float y, int width, int height) {
+        super(x, y, width, height);
+        locationA = x;
+        locationB = x + 100;
+    }
+
+    public abstract void drawCharacter(Graphics g); // abstract method forces implementation of enemy appearance for each enemy type
+
+    public void movement(float speed) {
+        // horizontally move enemy back and forth
+        if (forward == true)
+            x = x + speed;
+        else
+            x = x - speed;
+        // check if enemy should move forward or backward
+        if (x == locationB)
+            forward = false;
+        else if (x == locationA)
+            forward = true;
+    }
 }
+
+
